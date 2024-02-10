49a28b5445c675727d1e73d0e62460588481ac85
Trimpta <68428753+trimpta@users.noreply.github.com>
2024-02-11 01:40:34 +0530

    i will fall into my deep slumber now
    goodnight cruel world

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 #17:42 10-02-2024
 import math
 import pygame
-
+import random
 class plane:
 
-    g = 1
+    g = 9.8
 
     def __init__(self, size:tuple[int,int], skewX:int = 0, skewY:int = 0, contents:list = []) -> None:
         
@@ -16,45 +16,94 @@ class plane:
 
         self.sizeX, self.sizeY = size
         self.contents = contents
+        self.positions = []
+
+    def maxAngle(self):
+        self.skewX = 90 if self.skewX > 90 else self.skewX
+        self.skewX = -90 if self.skewX < -90 else self.skewX
+        self.skewY = 90 if self.skewY > 90 else self.skewY
+        self.skewY = -90 if self.skewY < -90 else self.skewY
 
     def setAngle(self, x, y):
         self.skewX, self.skewY = x, y
+        self.maxAngle()
     
     def addAngle(self, x, y):
         self.skewX += x
         self.skewX += y
+        self.maxAngle()
 
     def force(self):
         self.forceX = plane.g * math.sin(self.skewX)
         self.forceY = plane.g * math.sin(self.skewY)
 
         return (self.forceX, self.forceY)
+    
+    def indicator(self,screen):
+        w, h = pygame.display.Info().current_w, pygame.display.Info().current_h
+        unitX, unitY = w/90, h/90
+        center = (round(w/2),round(h/2))
 
+        end_pos = (round(self.skewX*unitX+center[0]), round(self.skewY*unitY+center[1]))
+        pygame.draw.line(screen,(50,50,100), center, (end_pos))
 
 class dust:
 
-    def __init__(self, plane, pos:tuple[int,int], mass:int = 1) -> None:
+    id = 0
+
+    def __init__(self, plane, pos:tuple[int,int], mass:int = 1, color:str = False, vel:tuple[int,int] = (0, 0)) -> None:
         
         self.plane = plane
         self.mass = mass
         self.posX,self.posY = pos
+        self.velX, self.velY = vel
         self.mass = mass
+        self.id = dust.id
+        dust.id +=1
+
+        if color:
+            self.color = color
+        else:
+            self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
 
         self.plane.contents.append(self)
 
         if not (0<self.posX<self.plane.sizeX or 0<self.posY<self.plane.sizeY):
             raise ValueError('dust must be inside your plane!!!')
         
-    def updatePos(self, deltaTime, force):
-
-        dx = self.posX + 0.5*force[0]*deltaTime**2/self.mass
-        dy = self.posY + 0.5*force[1]*deltaTime**2/self.mass
-
-        if 0<dx<self.plane.sizeX:
-            self.posX = dx
+    def pos(self):
+        return (math.floor(self.posX)*10,math.floor(self.posY)*10)
         
-        if 0<dy<self.plane.sizeY:
-            self.posY = dy
+    def updatePos(self, deltaTime, force, bug):
+
+        velX = self.velX + 0.5*force[0]*deltaTime**2/self.mass * 1000
+        velY = self.velY + 0.5*force[1]*deltaTime**2/self.mass * 1000
+
+        x = self.posX + velX
+        y = self.posY + velY
+
+        if not 0.1<x<self.plane.sizeX-0.1:
+                x = self.posX
+        
+        if not 0.1<y<self.plane.sizeY-0.1:
+                y = self.posY
+
+        newPos = (math.floor(x)*10, math.floor(y)*10)
+        if bug:
+            print('update', self.id, newPos, self.plane.positions)
+
+        for position in self.plane.positions:
+            if position[1] == newPos:
+                self.plane.positions.append((self.id,self.pos()))
+                print('a',self.id,position[0],self.pos())
+                return
+
+        self.posX, self.posY = x, y
+
+
+        self.plane.positions.append((self.id,self.pos()))
 
     def draw(self,screen):
-        pygame.draw.circle(screen, (255,255,255), (self.posX*10,self.posY*10), 5)
+        x,y = self.pos()
+        pygame.draw.rect(screen, self.color,(x,y,10,10))
--- main.py
+++ main.py
@@ -1,20 +1,64 @@
 from classes import *
+import time
 import pygame
 pygame.init()
 
-
+previous_time = time.time()
 height, width = 500,500
 screen = pygame.display.set_mode((width,height))
 
 surface = plane((width/10,height/10))
-surface.setAngle(-10,-25)
+surface.setAngle(30,10)
 
-particle = dust(surface, (15,15))
+particle1 = dust(surface, (0,4))
+particle2 = dust(surface, (0,3))
+# particle3 = dust(surface, (10,10))
 
+bug = False
 while True:
+
+
+    #Pygame events iteration
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
+
+        if event.type == pygame.MOUSEWHEEL:
+            if pygame.key.get_pressed()[pygame.K_LSHIFT]:
+                surface.addAngle(0,event.y)
+            else:
+                surface.addAngle(event.y, 0)
+
+    #Loop variables
+    deltaTime = time.time() - previous_time
+    previous_time = time.time()
     screen.fill((0,0,0))
     force = surface.force()
-    particle.updatePos(0.1, force)
-    particle.draw(screen)
-    print(particle.posX,particle.posY)
+    
+    #Particle updates
+    for particle in surface.contents:
+        particle.updatePos(deltaTime, force, bug)
+        particle.draw(screen)
+
+    surface.indicator(screen)
     pygame.display.update()
+
+    #debugging
+    if surface.positions == [(0, (0, 10)), (1, (0, 0))] and not bug:
+        bug = True
+
+    if bug:
+        # print(particle1.posX, particle1.posY)
+        pass
+
+    if surface.positions == [(0, (0, 0)), (1, (0, 0))]:
+        break
+
+
+    surface.positions = []
+
+while True:
+    if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+        pygame.quit()
+        break
